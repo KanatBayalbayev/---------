efa7980db22990ce9bb5526e47b1c563a43be775
Kanat Bayalbayev <Kanatkz07@mail.ru>
2022-11-28 20:53:26 +0600

    third commit

--- movieApp.js
+++ movieApp.js
@@ -4,9 +4,9 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-const main = document.getElementById("main");
-const form = document.getElementById("form");
-const search = document.getElementById("search");
+const containerEl = document.getElementById("main");
+const formEl = document.getElementById("form");
+const searchEl = document.getElementById("search");
 
 // initially get fav movies
 getMovies(APIURL);
@@ -22,7 +22,7 @@ async function getMovies(url) {
 
 function showMovies(movies) {
     // clear main
-    main.innerHTML = "";
+    containerEl.innerHTML = "";
 
     movies.forEach((movie) => {
         const { poster_path, title, vote_average, overview } = movie;
@@ -47,7 +47,7 @@ function showMovies(movies) {
             </div>
         `;
 
-        main.appendChild(movieEl);
+        containerEl.appendChild(movieEl);
     });
 }
 
@@ -61,14 +61,14 @@ function getClassByRate(vote) {
     }
 }
 
-form.addEventListener("submit", (e) => {
+formEl.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const searchTerm = search.value;
+    const searchTerm = searchEl.value;
 
     if (searchTerm) {
         getMovies(SEARCHAPI + searchTerm);
 
-        search.value = "";
+        searchEl.value = "";
     }
 });
